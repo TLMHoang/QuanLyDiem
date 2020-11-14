0182eeb65b74cd393e7df7b587d964e5180b8003
Tiến Ánh Nguyễn <it.nguyentienanh@gmail.com>
2020-11-14 23:54:44 +0700

    New

---
+++ AppQuanLyNhaTruong/DAL/LKHeLoaiDiemDAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DTO;
+
+namespace DAL
+{
+    public class LKHeLoaiDiemDAL : SQL.SQLHelper, CInterface<LKHeLoaiDiem>
+    {
+        DBQuanLyDiem db = new DBQuanLyDiem();
+        public Task<int> CapNhap(LKHeLoaiDiem obj)
+        {
+            db.TaiKhoanSinhViens.
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Lay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Lay(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Them(LKHeLoaiDiem obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Xoa(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppQuanLyNhaTruong/DAL/LoaiDiemDAL.cs
@@ -0,0 +1,58 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class LoaiDiemDAL : SQL.SQLHelper, CInterface<LoaiDiem>
+    {
+        public async Task<int> CapNhap(LoaiDiem obj)
+        {
+            return await ExecuteNonQuery(
+                "CapNhapLoaiDiem",
+                new SqlParameter("@ID", SqlDbType.BigInt) { Value = obj.ID },            
+                new SqlParameter("@Ten", SqlDbType.NVarChar) { Value = obj.Ten },            
+                new SqlParameter("@HeSo", SqlDbType.Float) { Value = obj.HeSo }
+                );
+        }
+
+        public async Task<DataTable> Lay()
+        {
+            return await ExecuteQuery(
+                "XemLoaiDiem",
+                new SqlParameter("@ID", SqlDbType.BigInt) { Value = -1 }
+                );
+        }
+
+        public async Task<DataTable> Lay(int ID)
+        {
+            return await ExecuteQuery(
+                "XemDiem",
+                new SqlParameter("@ID", SqlDbType.BigInt) { Value = ID }
+                );
+        }
+
+        public async Task<int> Them(LoaiDiem obj)
+        {
+            return await ExecuteNonQuery(
+                "ThemLoaiDiem",
+                new SqlParameter("@ID", SqlDbType.BigInt) { Value = obj.ID },
+                new SqlParameter("@Ten", SqlDbType.NVarChar) { Value = obj.Ten },
+                new SqlParameter("@HeSo", SqlDbType.Float) { Value = obj.HeSo }
+                );
+        }
+
+        public async Task<int> Xoa(int ID)
+        {
+            return await ExecuteNonQuery(
+                "XoaLoaiDiem",
+                new SqlParameter("@ID", SqlDbType.BigInt) { Value = ID }
+                );
+        }
+    }
+}
