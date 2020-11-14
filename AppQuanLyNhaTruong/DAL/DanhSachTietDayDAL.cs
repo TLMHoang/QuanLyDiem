f843ee461ef574bf14ed0bcd471fe6a437220134
Hy Phúc <nghuy9991@gmail.com>
2020-11-14 23:53:45 +0700

    123

---
+++ AppQuanLyNhaTruong/DAL/DanhSachTietDayDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DanhSachTietDayDAL : SQL.SQLHelper, CInterface<DanhSachTietDay>
+
+    {
+        Task<int> CInterface<DanhSachTietDay>.CapNhap(DanhSachTietDay obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Lay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataTable> Lay(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<int> CInterface<DanhSachTietDay>.Them(DanhSachTietDay obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Xoa(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
